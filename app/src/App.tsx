335682c59ef88f13dafbbb5b9c90b0b861c5dad4
vitorAugusto <vitoraug210@gmail.com>
2024-06-10 13:24:58 -0300

    criando componente de carrossel (mvp)

--- app/src/App.tsx
+++ app/src/App.tsx
@@ -1,14 +1,7 @@
-import { useState } from 'react';
 import React from 'react'
-import PageTitle from './components/pageTitle';
-import SideMenu from './components/sideMenu';
 import Carousels from "./components/carousel";
 
 function App() {
-  const [selectedMenuItem, setSelectedMenuItem] = useState('PERFIL')
-  const handleMenuItemClick = (menuItem: string) => {
-    setSelectedMenuItem(menuItem);
-  };
   return (
           <div>
               <h1>Meu Carrossel</h1>
