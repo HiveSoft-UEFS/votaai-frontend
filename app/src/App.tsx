943295d0368ddec55b09cd47f435aedc6e7c3cc0
LeticiaGSouza <124190885+Ltiicia@users.noreply.github.com>
2024-06-11 09:17:41 -0300

    Criando componente InfoCard

--- app/src/App.tsx
+++ app/src/App.tsx
@@ -2,6 +2,10 @@ import { useState } from 'react';
 import React from 'react'
 import PageTitle from './components/pageTitle';
 import SideMenu from './components/sideMenu'
+import InfoCard from './components/infoCard'
+import HowToVoteRoundedIcon from '@mui/icons-material/HowToVoteRounded';
+
+
 
 function App() {
   const [selectedMenuItem, setSelectedMenuItem] = useState('PERFIL')
@@ -21,6 +25,12 @@ function App() {
         selectedMenuItem={selectedMenuItem}
         onMenuItemClick={handleMenuItemClick}
       />
+
+      <InfoCard
+        title="Elaboração"
+        description="Crie, gerencie, compartilhe enquetes com quem quiser, de forma online, segura e confiável."
+        icon={<HowToVoteRoundedIcon style={{ fontSize: 50 }} />} //mudando icone e texto que se torna outros infocards
+    />
 
     </div>
   );
