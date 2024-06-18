a97e6071113e170599cb8f525be8a4ec3a703067
Elmer Carvalho <elmercarvalhofilho@gmail.com>
2024-06-18 11:26:47 -0300

    Componente BasePage Criado

--- app/src/App.tsx
+++ app/src/App.tsx
@@ -1,13 +1,16 @@
-import React from 'react'
+import React from 'react';
 import Carousels from "./components/carousel";
 import PageTitle from './components/pageTitle';
 import SideMenu from './components/sideMenu'
 import PollCard from "./components/pollCard";
 import RegistrationModal from "./components/RegistrationModal";
+import BasePage from './components/basePage';
+
+const initialMenuItem = 'PERFIL';
 
 function App() {
   return (
-      <RegistrationModal/>
+      <BasePage username='Roberto' title={initialMenuItem} />
   );
 }
 
