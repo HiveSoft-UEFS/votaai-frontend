844f2fc8fa244a96c06b161fd1b9b991b0d8c94e
Caio Bruno Santos Matos de Oliveira <kmbmatos2@gmail.com>
2024-06-20 01:28:00 -0300

    Adicionando o css na criação da enquete

--- app/src/App.tsx
+++ app/src/App.tsx
@@ -4,7 +4,7 @@ import PageTitle from './components/pageTitle';
 import SideMenu from './components/sideMenu'
 import PollCard from "./components/pollCard";
 import RegistrationModal from "./components/RegistrationModal";
-import CreatePoll from './pages/CreatePoll';
+import CreatePoll from './pages/CreatePollPage/CreatePoll';
 
 function App() {
   return (
