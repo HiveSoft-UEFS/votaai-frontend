97152c8852cde2b1c3f4e690883f2281b10af2c8
Caio Bruno Santos Matos de Oliveira <kmbmatos2@gmail.com>
2024-06-19 22:35:39 -0300

    Criando tela de criar enquete

--- app/src/pages/CreatePoll.jsx
+++ app/src/pages/CreatePoll.jsx
@@ -1,20 +1,12 @@
 import React, {useState} from "react";
 import PageTitle from "../components/pageTitle";
 import SideMenu from "../components/sideMenu";
+import BasePage from "../components/basePage";
 
 function CreatePoll(){
     const [titulo, setTitulo] = useState("");
     const [descricao, setDescricao] = useState("");
     const [dataLimite, setDataLimite] = useState(new Date());
-
-
-    const [selectedMenuItem, setSelectedMenuItem] = useState('CRIAR VOTAÇãO');
-    const userName = 'Caio Bruno';
-    const handleMenuItemClick = (menuItem) => {
-        setSelectedMenuItem(menuItem);
-    }
-
-
 
 
     const handleSubmit = (event) => {
@@ -25,11 +17,11 @@ function CreatePoll(){
     return (
         
         <div className="">
+
             <div>
-                <SideMenu
-                userName={userName}
-                selectedMenuItem={selectedMenuItem}
-                onMenuItemClick={handleMenuItemClick}
+                <BasePage username='Caio Bruno'
+                
+
                 />
             </div>
            
