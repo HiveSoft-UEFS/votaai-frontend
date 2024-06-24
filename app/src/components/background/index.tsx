38a755cd7ad1c8847820942fe9bd183ab34f39fa
Caio Bruno Santos Matos de Oliveira <kmbmatos2@gmail.com>
2024-06-24 19:59:50 -0300

    Merge branch 'main' of https://github.com/HiveSoft-UEFS/votaai-frontend

---
+++ app/src/components/background/index.tsx
@@ -0,0 +1,29 @@
+import React from "react";
+import "./background.css"
+
+
+const Background = () => {
+    return
+}
+
+export default Background;
+
+
+/*
+const Background = () => {
+    return (
+
+        <div className= "wavy-background">
+            <p>Tem algo!</p>
+            <svg id="visual" viewBox="0 0 960 540" width="960" height="540" xmlns="http://www.w3.org/2000/svg" xmlnsXlink="http://www.w3.org/1999/xlink" version="1.1">
+  {}
+            </svg>
+
+        </div>
+        
+            
+    )
+}
+
+export default Background
+*/
