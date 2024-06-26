261489c8648be2c82cba6c548948897366a19c26
vitorAugusto <vitoraug210@gmail.com>
2024-06-26 15:39:01 -0300

    pequenos ajustes

--- app/src/pages/searchPage/TelaPesquisa.tsx
+++ app/src/pages/searchPage/TelaPesquisa.tsx
@@ -1,9 +1,9 @@
 import React, { useState } from 'react';
-import PollCard from "../components/pollCard";
+import PollCard from "../../components/pollCard";
 import "./TelaPesquisa.css";
 import background from './background2.svg';
-import NavBar from "../components/navBar";
-import Footer from "../components/footer";
+import NavBar from "../../components/navbar/navbar";
+import Footer from "../../components/footer";
 import 'bootstrap/dist/css/bootstrap.min.css';
 
 const poll = [
