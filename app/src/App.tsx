5f6a033da4ec8209be48b7ed05cf46789cfa7c52
Elmer Carvalho <elmercarvalhofilho@gmail.com>
2024-06-28 09:31:44 -0300

    Ajustando Arquivos da Page CreatePoll

--- app/src/App.tsx
+++ app/src/App.tsx
@@ -4,11 +4,11 @@ import PageTitle from './components/pageTitle';
 import SideMenu from './components/sideMenu'
 import PollCard from "./components/pollCard";
 import RegistrationModal from "./components/RegistrationModal";
-import CreatePoll from './pages/CreatePollPage/CreatePoll';
+import BasePage from './components/basePage';
 
 function App() {
   return (
-    <CreatePoll/>
+    <BasePage username='pimpolho' title='PERFIL'/>
   );
 }
 
