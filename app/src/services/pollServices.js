d53bc4f3548022081d8defd9afabcf7945897254
Cláudia <96408566+clsf@users.noreply.github.com>
2024-07-18 11:48:40 -0300

    Update pollServices.js
    Retornando arquivo anterior

--- app/src/services/pollServices.js
+++ app/src/services/pollServices.js
@@ -1,8 +1,38 @@
 import axios from 'axios';
 
-const POLL_HISTORY_ENDPOINT = 'http://localhost:8000/polls/history/'; // Endpoint correto para histórico de enquetes
+const ENDPOINT = 'http://127.0.0.1:8000/polls';
+const POLL_HISTORY_ENDPOINT = 'http://localhost:8000/polls/history/';
 
-async function getUserPollHistory() {
+export const getPollData = async () => {
+    try {
+        const response = await axios.get(ENDPOINT);
+        console.log('Dados recebidos:', response.data);
+        return response.data;
+    } catch (error) {
+        console.error('Erro ao fazer requisição:', error);
+        throw error;
+    }
+};
+
+export const getPollSearch= async (value, order, category, tag, code) => {
+    try {
+        const response = await axios.get(`${ENDPOINT}/search/s`, {
+            params: {
+                value: value,
+                order: order,
+                category: category,
+                tag: tag,
+                code: code
+            }
+        });
+        return response.data;
+    } catch (error) {
+        console.error('Erro ao fazer requisição:', error);
+        throw error;
+    }
+};
+
+export const getUserPollHistory = async () => {
     try {
         const token = localStorage.getItem('accessToken');
         const headers = {
@@ -12,9 +42,7 @@ async function getUserPollHistory() {
         const response = await axios.get(POLL_HISTORY_ENDPOINT, { headers });
         return response.data;
     } catch (error) {
-        console.error('Error fetching poll history:', error);
+        console.error('Erro ao buscar histórico de enquetes:', error);
         throw error;
     }
-}
-
-export { getUserPollHistory };
+};
